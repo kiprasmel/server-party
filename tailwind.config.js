b1c18988d94d3efec6c2c9aa9a5a51b350d77943
Kipras Melnikovas <kipras@kipras.org>
2020-06-05 02:22:49 +0300

    Extend tailwind's color palette slightly
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
 	purge: [],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				/** ( ͡° ͜ʖ ͡°) */
+				"party-green": "#39e28d",
+				"party-purple": "#6400cf",
+				"party-black": "#222222",
+			},
+		},
 	},
 	variants: {},
 	plugins: [],
