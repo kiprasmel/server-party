d9c78cd06e9ffbf5f9c645a5deffabc3c77029b8
Kipras Melnikovas <kipras@kipras.org>
2020-06-04 21:24:31 +0300

    Setup react-router-dom
    See https://github.com/ReactTraining/react-router
    
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Login } from "./pages/Login";
 
 function App() {
-	return <div className="App" />;
+	return (
+		<>
+			<Router>
+				<div>
+					<Switch>
+						{/* TODO Landing? */}
+						<Route path="/" component={Login} />
+
+						<Route exact path="/login" component={Login} />
+					</Switch>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+export const Login: FC<{}> = () => {
+	const greeting = "Hello";
+	return (
+		<div>
+			<h1>{greeting}</h1>
+		</div>
+	);
+};
