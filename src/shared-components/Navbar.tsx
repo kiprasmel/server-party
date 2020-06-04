b344eeb6e4b36cf123599e8a31d20bc5e5b1b5c8
Kipras Melnikovas <kipras@kipras.org>
2020-06-05 02:25:43 +0300

    Create a basic version of the `Navbar` component
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ src/shared-components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+
+export const Navbar: FC = (props) => (
+	<nav {...props} className="w-10/12 mx-auto pt-4">
+		<ul className="flex text-2xl">
+			<li className="my-auto">
+				<h1 className="uppercase">
+					<Link to="/">Server Party</Link>
+				</h1>
+			</li>
+			<li className="ml-auto border border-gray-500 rounded">
+				<Link to="/login" className="inline-block px-4 py-2">
+					Log in
+				</Link>
+			</li>
+		</ul>
+	</nav>
+);
