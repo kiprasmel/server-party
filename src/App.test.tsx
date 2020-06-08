e4369e11b0baed6116e08b25744a168da87f8f50
Kipras Melnikovas <kipras@kipras.org>
2020-06-08 23:53:26 +0300

    test-xs App
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
 
-test("renders learn react link", () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+import "@testing-library/jest-dom/extend-expect"; /** typescript, autocompletions et al */
+
+import App from "./App";
+import { render } from "./test-utils";
+
+describe("App", () => {
+	it("Renders the Landing page", async () => {
+		const { getByText } = render(<App />);
+
+		expect(getByText("Let's party!")).toBeInTheDocument();
+	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,48 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { FC } from "react";
+import { Router, Switch, Route } from "react-router-dom";
 
-import { AuthContextWrapper } from "./contexts/AuthContext";
+import { History, createBrowserHistory } from "history";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Landing } from "./pages/Landing";
 import { Login } from "./pages/Login";
 import { Party } from "./pages/Party";
 import { PrivateRoute } from "./shared-components/PrivateRoute";
 import { NotFound404 } from "./pages/NotFound404";
+import { AuthDetails, localStorageAuthDetailsKey } from "./models/AuthDetails";
+import { getAuthDetails } from "./utils/auth";
 
-function App() {
-	return (
-		<>
-			<AuthContextWrapper>
-				<Router>
-					<Switch>
-						<Route exact path="/" component={Landing} />
-
-						<Route exact path="/login" component={Login} />
-
-						<PrivateRoute
-							exact
-							path="/party"
-							redirectHumanMsg="Looks like you'll have to login first!"
-							component={Party}
-						/>
-
-						<Route component={NotFound404} />
-					</Switch>
-				</Router>
-			</AuthContextWrapper>
-		</>
-	);
+interface AppOptions {
+	authDetailsLSKey?: string;
+	authDetails?: AuthDetails;
+	history?: History<History.PoorMansUnknown>;
 }
+
+const App: FC<AppOptions> = ({
+	authDetailsLSKey = localStorageAuthDetailsKey,
+	authDetails = getAuthDetails(authDetailsLSKey),
+	history = createBrowserHistory(),
+}) => (
+	<>
+		<AuthContextProvider authDetails={authDetails}>
+			<Router history={history}>
+				<Switch>
+					<Route exact path="/" component={Landing} />
+
+					<Route exact path="/login" component={Login} />
+
+					<PrivateRoute
+						exact
+						path="/party"
+						redirectHumanMsg="Looks like you'll have to login first!"
+						component={Party}
+					/>
+
+					<Route component={NotFound404} />
+				</Switch>
+			</Router>
+		</AuthContextProvider>
+	</>
+);
 
 export default App;
